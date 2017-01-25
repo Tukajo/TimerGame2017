d4bbcf0826dd30457cf7d341cfe9138aba8d4aa1
Timothy Frisch <Tjfrisch@gmail.com>
2017-01-24 20:32:05 -0600

    Changes on 1/24/2017 - Fix for color indicator bar not changing on correct button click. - Changes towards "smart weighting" low-time buttons.
    TODO:
    -  Rework how weights work in timer of ButtonWrapper in order to not overly-weight a button color incorrectly.
    - Initiate work with Firebase and saving user scores to the web.

--- app/src/main/java/com/example/tjfri/timergame2017/Gameplay.java
+++ app/src/main/java/com/example/tjfri/timergame2017/Gameplay.java
@@ -16,11 +16,12 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Gameplay extends AppCompatActivity implements ButtonWrapper.ButtonWrapperListener{
+    private String global_time_key = "game_time", correct_clicks_key = "correct_clicks";
     private ArrayList<ButtonWrapper> myButtonsArray = new ArrayList<ButtonWrapper>();
     private Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14, btn15, btn16, btn17, btn18, btn19, btn20, btn21, btn22, btn23, btn24, btn25;
     private ButtonWrapper button1,button2,button3,button4,button5,button6,button7,button8,button9,button10,button11,button12,button13,button14,button15,button16,button17,button18,button19,button20,button21,button22,button23,button24,button25;
     private TextView indicatorBar;
-    private int timeInMs = 30000, tickInterval = 1000;
+    private int correctSelections=0;
     private Indicator indicatorBarWrapper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,33 +55,35 @@ public class Gameplay extends AppCompatActivity implements ButtonWrapper.ButtonW
         btn23 = (Button) findViewById(R.id.button23);
         btn24 = (Button) findViewById(R.id.button24);
         btn25 = (Button) findViewById(R.id.button25);
+        //Create indicator bar object.
+        indicatorBarWrapper = new Indicator(this,getApplicationContext(),indicatorBar);
 
         //Construct wrappers
-        button1 = new ButtonWrapper(getApplicationContext(),btn1,timeInMs,tickInterval,this);
-        button2 = new ButtonWrapper(getApplicationContext(),btn2,timeInMs,tickInterval,this);
-        button3 = new ButtonWrapper(getApplicationContext(),btn3,timeInMs,tickInterval,this);
-        button4 = new ButtonWrapper(getApplicationContext(),btn4,timeInMs,tickInterval,this);
-        button5 = new ButtonWrapper(getApplicationContext(),btn5,timeInMs,tickInterval,this);
-        button6 = new ButtonWrapper(getApplicationContext(),btn6,timeInMs,tickInterval,this);
-        button7 = new ButtonWrapper(getApplicationContext(),btn7,timeInMs,tickInterval,this);
-        button8 = new ButtonWrapper(getApplicationContext(),btn8,timeInMs,tickInterval,this);
-        button9 = new ButtonWrapper(getApplicationContext(),btn9,timeInMs,tickInterval,this);
-        button10 = new ButtonWrapper(getApplicationContext(),btn10,timeInMs,tickInterval,this);
-        button11 = new ButtonWrapper(getApplicationContext(),btn11,timeInMs,tickInterval,this);
-        button12 = new ButtonWrapper(getApplicationContext(),btn12,timeInMs,tickInterval,this);
-        button13 = new ButtonWrapper(getApplicationContext(),btn13,timeInMs,tickInterval,this);
-        button14 = new ButtonWrapper(getApplicationContext(),btn14,timeInMs,tickInterval,this);
-        button15 = new ButtonWrapper(getApplicationContext(),btn15,timeInMs,tickInterval,this);
-        button16 = new ButtonWrapper(getApplicationContext(),btn16,timeInMs,tickInterval,this);
-        button17 = new ButtonWrapper(getApplicationContext(),btn17,timeInMs,tickInterval,this);
-        button18 = new ButtonWrapper(getApplicationContext(),btn18,timeInMs,tickInterval,this);
-        button19 = new ButtonWrapper(getApplicationContext(),btn19,timeInMs,tickInterval,this);
-        button20 = new ButtonWrapper(getApplicationContext(),btn20,timeInMs,tickInterval,this);
-        button21 = new ButtonWrapper(getApplicationContext(),btn21,timeInMs,tickInterval,this);
-        button22 = new ButtonWrapper(getApplicationContext(),btn22,timeInMs,tickInterval,this);
-        button23 = new ButtonWrapper(getApplicationContext(),btn23,timeInMs,tickInterval,this);
-        button24 = new ButtonWrapper(getApplicationContext(),btn24,timeInMs,tickInterval,this);
-        button25 = new ButtonWrapper(getApplicationContext(),btn25,timeInMs,tickInterval,this);
+        button1 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn1,this);
+        button2 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn2,this);
+        button3 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn3,this);
+        button4 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn4,this);
+        button5 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn5,this);
+        button6 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn6,this);
+        button7 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn7,this);
+        button8 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn8,this);
+        button9 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn9,this);
+        button10 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn10,this);
+        button11 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn11,this);
+        button12 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn12,this);
+        button13 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn13,this);
+        button14 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn14,this);
+        button15 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn15,this);
+        button16 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn16,this);
+        button17 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn17,this);
+        button18 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn18,this);
+        button19 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn19,this);
+        button20 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn20,this);
+        button21 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn21,this);
+        button22 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn22,this);
+        button23 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn23,this);
+        button24 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn24,this);
+        button25 = new ButtonWrapper(getApplicationContext(),indicatorBarWrapper,btn25,this);
 
         myButtonsArray.add(button1);
         myButtonsArray.add(button2);
@@ -108,8 +111,6 @@ public class Gameplay extends AppCompatActivity implements ButtonWrapper.ButtonW
         myButtonsArray.add(button24);
         myButtonsArray.add(button25);
 
-        //Create indicator bar object.
-        indicatorBarWrapper = new Indicator(this,getApplicationContext(),indicatorBar);
         checkGameValidity();
     }
 
@@ -150,7 +151,7 @@ public class Gameplay extends AppCompatActivity implements ButtonWrapper.ButtonW
     @Override
     public void buttonPressed(ButtonWrapper buttonWrapper) {
         if(checkButtonColorIndicatorMatch(buttonWrapper.getCurrentColor(),indicatorBarWrapper.getCurrentColor())){
-            Toast.makeText(getApplicationContext(),"Good job!",Toast.LENGTH_LONG).show();
+            //Toast.makeText(getApplicationContext(),"Good job!",Toast.LENGTH_LONG).show();
             doCorrectMatchWork(buttonWrapper);
         }else{
             doIncorrectMatchWork(buttonWrapper);
@@ -159,7 +160,7 @@ public class Gameplay extends AppCompatActivity implements ButtonWrapper.ButtonW
 
     @Override
     public void gameLost(ButtonWrapper lostButton) {
-        Toast.makeText(getApplicationContext(),"You lost the game",Toast.LENGTH_LONG).show();
+        //Toast.makeText(getApplicationContext(),"You lost the game",Toast.LENGTH_LONG).show();
         indicatorBarWrapper.stopGameTimer();
         //Transfer to new game activity.
         haltGame();
@@ -167,20 +168,28 @@ public class Gameplay extends AppCompatActivity implements ButtonWrapper.ButtonW
     }
     public void transferGameState(){
         Bundle bundle = new Bundle();
-        bundle.putInt("Game Time",indicatorBarWrapper.getGlobalTime());
+        bundle.putInt(global_time_key,indicatorBarWrapper.getGlobalTime());
+        bundle.putInt(correct_clicks_key, correctSelections);
         Intent lossScreenTransferIntent = new Intent(getBaseContext(), LossScreen.class);
         lossScreenTransferIntent.putExtras(bundle);
         startActivity(lossScreenTransferIntent);
     }
 
     public void doCorrectMatchWork(ButtonWrapper correctButtonFound){
+        correctSelections++;
         correctButtonFound.randomizeBtnColor();
         correctButtonFound.resetTimer();
-        indicatorBarWrapper.randomizeBarColor();
+        indicatorBarWrapper.determineWeightedPick();
         checkGameValidity();
     }
 
     public void doIncorrectMatchWork(ButtonWrapper incorrectButtonFound){
         incorrectButtonFound.speedUpTimer();
     }
+    @Override
+    public void onBackPressed() {
+        haltGame();
+        super.onBackPressed();
+    }
+
 }
